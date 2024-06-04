9893e523dd91276dcd5c7d38a200e9416b461796
ManaswiniAndru <146472799+ManaswiniAndru@users.noreply.github.com>
2024-06-04 10:53:56 +0530

    uploading day 1 file

---
+++ 1.py
@@ -0,0 +1,338 @@
+'''a#=20
+print(a#)'''
+
+'''def=10
+print(def)'''
+
+'''reserved words:
+    True ,False,None and,while,continue,return,from,as,class,def,pass,global,
+    lambda,for,nonlocal,assert,import,del,with,break,if,elif,else,and,or''' 
+
+'''INBULIT FUNCTIONS:
+    TYPE()
+    ID()
+    PRINT()
+
+a=20
+print(a)
+print(id(a))
+print(type(a))'''
+
+''' NUMERIC DATA TYPES:
+int
+float
+complex'''
+#MANASWINI
+'''SLICEING STRING:
+    
+str="chandana"
+print(str[1])
+
+print(str[2:13:3])'''
+
+'''a=10.5
+print(int(a))'''
+
+'''a=int(input("enter the number:"))
+print(a)
+print(type(a))'''
+
+'''a=10
+print(id(a))
+
+a=20
+print(id(20))'''
+
+'''LIST DATA TYPE:
+list1=[2,3,"day","true",9.99]
+print(list1)
+print(list1[-1])
+list1.append("cat")#{to add}
+list1.remove("day")
+print(list1)'''
+
+#TUPLE DATA TYPE:
+
+'''tuple1=(2,3,"dog","cat",9.9)
+print(tuple1)
+#no append and remove
+
+set1={10,20,10,"dog","true","pig"}
+print(set1)'''# set is un ordered
+
+#OPERETORS:
+#ARTHMETIC OPERATORS(+,-,/,*,%)
+
+'''a=10
+b=20
+print(a+b)
+# *5/2 is 2.5 but5//2 is 2
+
+
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+y=int(input("enter the number:"))
+print(a+b)
+print(a-y)
+print(a//b)
+print(a**y)'''
+
+'''a=int(input("enter the number:"))
+if a%3==0:
+    print("yes")
+    
+else:
+    print("no")'''
+
+#BITWISE OPERATORS:(&,^,|,~,<<,>>)
+#(&)= both 1 and AT least one 0),
+#(|)=at least one 1,
+#(~)=(1-->0 0-->1)
+
+#SWAP:
+'''a=12
+b=13
+temp=b
+c=a
+a=b
+b=c
+print(a)
+print(b)'''
+
+#SWAP TO NUMBERS USING XOR:
+#a=5
+#b=7
+
+'''a=5
+b=7
+a=a^b
+b=a^b
+a=a^b
+print(a)
+print(b)'''
+
+'''a=7
+print(-(~a))'''#~
+
+
+'''a=int(input("enter the value:"))
+if(a&1==0):
+    print("even")
+else:
+    print("odd")'''#BITWISE
+
+#ASSIGNMENT OPERATORS
+
+'''+=
+-=
+*=
+/=
+%=
+//=
+**=
+&='''
+
+'''a=int(input("enter the value:"))
+if(a>0):
+    print("positive integer")
+elif(a<0):
+    print("negative integer")
+else:
+    print("0")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
